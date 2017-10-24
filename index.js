4949a0646e47383e040c2dda55b1129b431f412c
Diego Rodríguez <diegorbaquero@gmail.com>
2017-10-24 18:55:26 -0500

    fix: options not used

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const findById = (model, pk = 'id') => (id, options) =>
+const findById = (model, pk = 'id') => (id, options = {}) =>
   new Promise(async (resolve, reject) => {
-    const result = await model.findOne({where: {[pk]: id}})
+    const opts = Object.assign(options, {where: {[pk]: id}})
+    const result = await model.findOne(opts)
     if (result === null) {
       const err = new Error(`${model.name} with ${pk} ${id} does not exist`)
       err.status = 404
