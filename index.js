428b1c1d3fda6e8098d47a552ebfdfbb6b7b0e48
Diego Rodríguez <diegorbaquero@gmail.com>
2017-12-26 16:08:53 -0500

    Use async function directly

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const findById = (model, pk = 'id') => (id, options = {}) =>
-  new Promise(async (resolve, reject) => {
+const findById = (model, pk = 'id') => async (id, options = {}) => {
     const opts = Object.assign(options, {where: {[pk]: id}})
     const result = await model.findOne(opts)
     if (result === null) {
       const err = new Error(`${model.name} with ${pk} ${id} does not exist`)
       err.status = 404
-      reject(err)
+      throw err
     }
-    resolve(result)
-  })
+    return result
+  }
 
 module.exports = findById
