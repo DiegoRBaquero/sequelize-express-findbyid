6e0f0cde81d573ddd77ffde300b62ddee70fcd91
Diego Rodríguez <diegorbaquero@gmail.com>
2018-02-01 15:56:41 -0500

    Default where fix

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const findById = (model, pk = 'id') => async (id, options = {where: {}}) => {
-    const opts = Object.assign(options.where, {[pk]: id})
-    const result = await model.findOne(opts)
-    if (result === null) {
-      const err = new Error(`${model.name} with ${pk} ${id} does not exist`)
-      err.status = 404
-      throw err
-    }
-    return result
+const findById = (model, pk = 'id') => async (id, options = {}) => {
+  if (!options.where) options.where = {}
+  const opts = Object.assign(options.where, {[pk]: id})
+  const result = await model.findOne(opts)
+  if (result === null) {
+    const err = new Error(`${model.name} with ${pk} ${id} does not exist`)
+    err.status = 404
+    throw err
   }
+  return result
+}
 
 module.exports = findById
