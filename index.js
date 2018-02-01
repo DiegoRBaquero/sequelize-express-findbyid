bd257c217789899d38f63c85113bd2c543c3ec65
Diego Rodríguez <diegorbaquero@gmail.com>
2018-02-01 15:50:03 -0500

    Use explicit where in options

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const findById = (model, pk = 'id') => async (id, options = {}) => {
-    const opts = Object.assign(options, {where: {[pk]: id}})
+    const opts = Object.assign(options.where, {[pk]: id})
     const result = await model.findOne(opts)
     if (result === null) {
       const err = new Error(`${model.name} with ${pk} ${id} does not exist`)
